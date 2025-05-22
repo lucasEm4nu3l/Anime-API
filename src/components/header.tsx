34e45d92455e1e59d0d7672c4c912c1195f59187
lucasEm4nu3l <lucasb.silva130@gmail.com>
2025-05-21 21:03:10 -0300

    Aprimoramento do código e organização da home, como também criação da página sobre

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,37 +5,38 @@ import { Link } from "react-router-dom"
 export const Header = () => {
   return (
         <header style={{
-            backgroundColor: "#DC143C",
+            backgroundImage: "url('/public/79075d5a8667cafe0845b3333cc42ebc.jpg')",
             padding: "2rem",
             textAlign: "center",
             fontWeight: "bold",
-            fontSize: "25px",
-             
+            fontSize: "25px",  
         }}>
-            <img src="../../public/XwI4.gif" style={{
-                width: "6.25rem",
-                alignItems: "center",   
-            }}/>
-            <nav style={{
+                <nav style={{
                 display: "flex",
                 justifyContent: "center",
                 gap: "2rem",
                 padding: "1rem",
+                backgroundColor: "#040714",
+                width: "25%", 
+                alignItems: "center",
+                marginLeft: "38%",
+                boxShadow: "0 4px 8px rgba(0,0,0,0.5)",
+                borderRadius: "8px",
+                opacity: "70%"
             }}>
             <Link style={{
                 textDecoration: "none",
-                color: "inherit",    
+                color: "white",    
             }}  to={"/"}>Home</Link>
             <Link style={{
                 textDecoration: "none",
-                color: "inherit",    
+                color: "white",    
             }} to={"/sobre"}>Sobre</Link>
             <Link style={{
                 textDecoration: "none",
-                color: "inherit",    
+                color: "white",    
             }} to={"/contato"}>Contato</Link>
             </nav>
-            
         </header>
   )
 }
--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,12 +1,26 @@
-import { Box } from "@mui/material"
+import { Box, Container, Paper } from "@mui/material"
 
 
 export const Sobre = () => {
-  return (
-    <div>
-        <Box>
-            
-        </Box>
-    </div>
-  )
+return (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#040714",
+    }}
+  >
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ padding: 4, color: "#fff", backgroundColor: "#1a1a2e" }}>
+        <p>
+          API feita para fornecer informações completas sobre animes, como título, sinopse,
+          imagem, episódios, status e avaliações. Permite buscar animes por nome ou ID,
+          facilitando a criação de catálogos e apps relacionados ao universo anime.
+        </p>
+      </Paper>
+    </Container>
+  </Box>
+);
 }
